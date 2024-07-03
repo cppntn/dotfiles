0d00dc8bcd9e4eed2bda348c810fa98df101a1d0
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-07-03 14:26:18 +0200

    put snippets back

--- .config/nvim/lua/plugins/syntax.lua
+++ .config/nvim/lua/plugins/syntax.lua
@@ -100,13 +100,13 @@ return {
     "hrsh7th/cmp-nvim-lsp"
   },
   -- luasnip
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   dependencies = {
-  --     "saadparwaiz1/cmp_luasnip",
-  --     "rafamadriz/friendly-snippets"
-  --   }
-  -- },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets"
+    }
+  },
   -- completions
   {
     "hrsh7th/nvim-cmp",
@@ -126,13 +126,13 @@ return {
     },
     config = function()
       local cmp = require("cmp")
-      -- require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
-        -- snippet = {
-        --   expand = function(args)
-        --     require('luasnip').lsp_expand(args.body)
-        --   end
-        -- },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
@@ -141,12 +141,12 @@ return {
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
+          ['<ESC>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
-          -- { name = 'luasnip' }
+          { name = 'luasnip' }
         }, {
           { name = 'buffer' }
         })
