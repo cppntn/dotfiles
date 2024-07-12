3dad9ffe56e811a3b10d8a575720ff7506193e98
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-07-12 17:52:07 +0200

    add barbar instead of cokelie; 'ctrl+b <' to set pane name in tmux

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -2,13 +2,80 @@ return {
   -- indent-blankline
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   -- bufferline
+  -- {
+  --   "willothy/nvim-cokeline",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",       -- Required for v0.4.0+
+  --     "nvim-tree/nvim-web-devicons", -- If you want devicons
+  --   },
+  --   config = true
+  -- }
   {
-    "willothy/nvim-cokeline",
+    'romgrk/barbar.nvim',
     dependencies = {
-      "nvim-lua/plenary.nvim",       -- Required for v0.4.0+
-      "nvim-tree/nvim-web-devicons", -- If you want devicons
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
-    config = true
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      animation = false,
+      icons = {
+        -- Configure the base icons on the bufferline.
+        -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+        buffer_index = false,
+        buffer_number = false,
+        button = '',
+        -- Enables / disables diagnostic symbols
+        diagnostics = {
+          [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+          [vim.diagnostic.severity.WARN] = { enabled = false },
+          [vim.diagnostic.severity.INFO] = { enabled = false },
+          [vim.diagnostic.severity.HINT] = { enabled = true },
+        },
+        gitsigns = {
+          added = { enabled = true, icon = '+' },
+          changed = { enabled = true, icon = '~' },
+          deleted = { enabled = true, icon = '-' },
+        },
+        filetype = {
+          -- Sets the icon's highlight group.
+          -- If false, will use nvim-web-devicons colors
+          custom_colors = false,
+
+          -- Requires `nvim-web-devicons` if `true`
+          enabled = true,
+        },
+        separator = { left = '▎', right = '' },
+
+        -- If true, add an additional separator at the end of the buffer list
+        separator_at_end = true,
+
+        -- Configure the icons on the bufferline when modified or pinned.
+        -- Supports all the base icon options.
+        modified = { button = '●' },
+        pinned = { button = '', filename = true },
+
+        -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+        preset = 'powerline',
+
+        -- Configure the icons on the bufferline based on the visibility of a buffer.
+        -- Supports all the base icon options, plus `modified` and `pinned`.
+        alternate = { filetype = { enabled = false } },
+        current = { buffer_index = true },
+        inactive = { button = '×' },
+        visible = { modified = { buffer_number = false } },
+      },
+
+      -- If true, new buffers will be inserted at the start/end of the list.
+      -- Default is to insert after current buffer.
+      insert_at_end = true,
+      sidebar_filetypes = {
+        -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+        NvimTree = true
+      }
+
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
   -- catppuccin theme
   {
